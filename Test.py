cb78ceca4243a40739d1958ce67adaecb9212ca7
Doan Chi Thien <18H1040056@ut.edu.vn>
2021-03-06 14:38:49 +0700

    add new modify Test.py

--- Test.py
+++ Test.py
@@ -3,6 +3,9 @@ arr=[]
 for i in range(0,n):
     x=int(input("nhap phan tu thu %d: " % (i)))
     arr.append(x)
-print('mang da nhap la :%d ')
+print('mang da nhap la : ')
+print(arr)
+arr.sort()
+print('mang tang dan la: ')
 print(arr)
             
